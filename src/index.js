b79c38090bddeb8bfe2bae78c73d6a3db2c14319
Miguel Diaz <bladdenemes@gmail.com>
2022-01-12 00:23:12 +0000

    Prueba 3

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ main();*/
 const app = require('./appmateria');
 const dbConnect = require ('../src/database.js');
 
-var port = (process.env.PORT || 1600);
+var port = (process.env.PORT || 3000);
 
 console.log("Starting API server on port: " + port);
 
