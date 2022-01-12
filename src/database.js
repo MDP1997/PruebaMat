06230583db7b46b6091703bd9ca994e8e56024c8
Miguel Diaz <bladdenemes@gmail.com>
2022-01-12 10:36:25 +0000

    Activar BD

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const DB_URL = process.env.MONGO_URL || "mongodb://localhost:27017/materiasDB";
-//const DB_URL = process.env.MONGO_URL || "mongodb://mongodb:27017";
+//const DB_URL = process.env.MONGO_URL || "mongodb://localhost:27017/materiasDB";
+const DB_URL = process.env.MONGO_URL || "mongodb://mongodb:27017";
 
 const dbConnect = function () {
   const db = mongoose.connection;
