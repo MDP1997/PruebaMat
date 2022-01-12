55ed3ff7d33447a424bc08a09d527b7acb6ab476
Miguel Diaz <bladdenemes@gmail.com>
2022-01-12 10:14:08 +0000

    Corrigiendo error docker

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,6 @@ RUN npm install
 COPY . .
 
 
-EXPOSE 1600
+EXPOSE 3000
 
 CMD npm start
--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ main();*/
 const app = require('./appmateria');
 const dbConnect = require ('../src/database.js');
 
-var port = (process.env.PORT || 2999);
+var port = (process.env.PORT || 3000);
 
 console.log("Starting API server on port: " + port);
 
